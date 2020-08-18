3d651c6713cf11a50ff02b121ad620b3e9ffea4f
CaliGirl-sb <65486348+CaliGirl-sb@users.noreply.github.com>
2020-08-17 20:59:46 -0700

    Add files via upload

---
+++ BiggerNumber.py
@@ -0,0 +1,10 @@
+print("Enter x: ")
+x = int(input())
+print("Enter y: ")
+y = int(input())
+if x>y:
+    print("1=Первое число больше второго")
+elif x<y:
+    print("2=Второе число больше первого")
+else:
+    print("0=Числа равны")
